fd8d6eb70c9abf61158bdea20fcfa9dd4682403c
Zoli1212 <43321960+Zoli1212@users.noreply.github.com>
2020-10-18 16:29:53 +0200

    first commit

---
+++ base/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import JsonResponse
+import stripe
+
+
+# Create your views here.
+
+def index(request):
+    
+	return render(request, 'base/index.html')
+
+
+def charge(request):
+    	
+	amount = 5
+	if request.method == 'POST':
+		print('Data:', request.POST)
+
+		amount = int(request.POST['amount'])
+
+		customer = stripe.Customer.create(
+			email = request.POST['email'],
+			name=request.POST['nickname'],
+			source = request.POST['stripeToken'],
+		)
+		charge = stripe.Charge.create(
+			customer = customer,
+			amount = amount*100,
+			currency='usd',
+			description="Donation"
+		)
+
+	return redirect(reverse('success', args=[amount]))
+
+
+def successMsg(request, args):
+	amount = args
+	return render(request, 'base/success.html', {'amount':amount})
